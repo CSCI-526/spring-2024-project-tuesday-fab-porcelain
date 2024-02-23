97fb4d26043d33d77ee3ecb004f35a93d6fd6b23
Nuo Ma <nuoma2000@gmail.com>
2024-02-22 17:47:23 -0800

    Trick1 publish

--- Assets/Script/Trick1.cs
+++ Assets/Script/Trick1.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Trick1 : MonoBehaviour
 {
+    public GameObject wall;
+    private Vector3 scaleChange, positionChange;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        GameObject child = other.GetComponent<Collider2D>().gameObject;
-        Debug.Log("in ");
-
-        if(child.name == "Button"){
-            Debug.Log("hit botton");
-        }
+        scaleChange = new Vector3(-wall.transform.localScale.x / 2, 0.0f, 0.0f);
+        positionChange = new Vector3(0.0f, -wall.transform.localScale.x / 4, 0.0f);
+        wall.transform.localScale += scaleChange;
+        wall.transform.localPosition += positionChange;
     }
 }
