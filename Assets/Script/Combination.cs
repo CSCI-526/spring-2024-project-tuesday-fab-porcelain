c706ab4570d93c9d99ef7254d8e7a32baf9fb33b
yuanzhangusc <yzhang61@usc.edu>
2024-02-25 03:10:59 -0800

    version2

--- Assets/Script/Combination.cs
+++ Assets/Script/Combination.cs
@@ -5,72 +5,99 @@ using UnityEngine;
 public class Combination : MonoBehaviour
 {
 
+    // 杆的两端对象
     public GameObject playerA;
     public GameObject playerB;
-    public int curCombination = 1;
-    [SerializeField] private GameObject combinationPrefab;
+    public Vector3 playerAPosition;
+    public Vector3 playerBPosition;
+    //控制跳跃力量
+    public float jumpForce = 260f;
+    // 刚体
+    Rigidbody2D mRigidbody2D;
 
+    //public int curCombination = 1;
+    //[SerializeField] private GameObject combinationPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        CombinationSwitch();
+        // 获取刚体组件
+        mRigidbody2D = GetComponent<Rigidbody2D>();
+        //CombinationSwitch();
     }
 
-    // Update is called once per frame
-    void Update()
+    //void CombinationSwitch()
+    //{
+    //    //TODO: add more combinations
+    //    if (curCombination == 1)
+    //    {
+    //        Instantiate(combinationPrefab, (playerA.transform.position + playerB.transform.position) / 2, playerA.transform.rotation, this.transform);
+    //        gameObject.AddComponent<Rigidbody2D>();
+    //    }
+    //}
+
+    //FixedUpdate()是Unity自有函数[固定间隔时间内调用](类似于update函数),这里主要用来处理物理计算
+    void FixedUpdate()
     {
-        //TODO: press space to switch combination
-        //TODO: add different update functions for each combination
-        if(curCombination == 1){
-            RodUpdate();
-        }
-    }
-
-    void CombinationSwitch()
-    {
-        //TODO: add more combinations
-        if(curCombination == 1){
-            Instantiate(combinationPrefab, (playerA.transform.position + playerB.transform.position) / 2, playerA.transform.rotation, this.transform);
-            gameObject.AddComponent<Rigidbody2D>();
-        }
-
-    }
-
-    void RodUpdate()
-    {
-        // fetch rigidbody
-        Rigidbody2D mRigidbody2D = GetComponent<Rigidbody2D>();
-
-        // 当上箭头松开时, 给playerB 的位置施加一个Y 轴上的150 单位的力
-        if(Input.GetKeyUp(KeyCode.UpArrow)) {
-            mRigidbody2D.AddForceAtPosition(new Vector2(0.0f, 150.0f), playerB.transform.position, ForceMode2D.Force);
-        }
-
-        // 当左箭头按下时, 给playerB 的位置施加一个X 轴上的-5 单位的力
-        if(Input.GetKey(KeyCode.LeftArrow)) {
-            mRigidbody2D.AddForceAtPosition(new Vector2(-5.0f, 0.0f), playerB.transform.position, ForceMode2D.Force);
-        }
-
-        // 当右箭头按下时, 给playerB 的位置施加一个X 轴上的5 单位的力
-        if(Input.GetKey(KeyCode.RightArrow)) {
-            mRigidbody2D.AddForceAtPosition(new Vector2(5.0f, 0.0f), playerB.transform.position, ForceMode2D.Force);
-        }
-
-
-        // 当W 松开时, 给playerA 的位置施加一个Y 轴上的150 单位的力
-        if(Input.GetKeyUp(KeyCode.W)) {
-            mRigidbody2D.AddForceAtPosition(new Vector2(0.0f, 150.0f), playerA.transform.position, ForceMode2D.Force);
-        }
-
-        // 当A 按下时, 给playerA 的位置施加一个X 轴上的-5 单位的力
-        if(Input.GetKey(KeyCode.A)) {
+        // 当A键按下时, 给playerA 的位置施加一个X 轴上的-5 单位的力
+        if (Input.GetKey(KeyCode.A))
+        {
             mRigidbody2D.AddForceAtPosition(new Vector2(-5.0f, 0.0f), playerA.transform.position, ForceMode2D.Force);
         }
 
-        // 当D 按下时, 给playerA 的位置施加一个X 轴上的5 单位的力
-        if(Input.GetKey(KeyCode.D)) {
+        // 当D键 按下时, 给playerA 的位置施加一个X 轴上的5 单位的力
+        if (Input.GetKey(KeyCode.D))
+        {
             mRigidbody2D.AddForceAtPosition(new Vector2(5.0f, 0.0f), playerA.transform.position, ForceMode2D.Force);
         }
+
+        // 当左箭头 按下时, 给playerB 的位置施加一个X 轴上的-5 单位的力
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            mRigidbody2D.AddForceAtPosition(new Vector2(-5.0f, 0.0f), playerB.transform.position, ForceMode2D.Force);
+        }
+
+        // 当右箭头 按下时, 给playerB 的位置施加一个X 轴上的5 单位的力
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            mRigidbody2D.AddForceAtPosition(new Vector2(5.0f, 0.0f), playerB.transform.position, ForceMode2D.Force);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // 
+        // //TODO: press space to switch combination
+        // //TODO: add different update functions for each combination
+        // if(curCombination == 1){
+        //     RodUpdate();
+        // }
+
+        // Debug.Log(playerA.transform.position + "   " + playerB.transform.position);
+        playerAPosition = playerA.transform.position;
+        playerBPosition = playerB.transform.position;
+
+        // 当 W键 松开时, 给playerA 的位置施加一个Y 轴上的150 单位的力
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            if (playerA.transform.position.y <= -0.62f)
+            {
+                //Debug.Log("Will add force to player A");
+                mRigidbody2D.AddForceAtPosition(new Vector2(0.0f, jumpForce), playerA.transform.position, ForceMode2D.Force);
+            }
+        }
+
+        // 当上箭头 松开时, 给playerB 的位置施加一个Y 轴上的150 单位的力
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            if (playerB.transform.position.y <= -0.62f)
+            {
+                //Debug.Log("Will add force to player B");
+                mRigidbody2D.AddForceAtPosition(new Vector2(0.0f, jumpForce), playerB.transform.position, ForceMode2D.Force);
+            }
+        }
+
+
     }
 }
