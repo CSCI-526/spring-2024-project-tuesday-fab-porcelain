078eae20e26c6a484e9a96a5122459a113d79736
erica <erica.yywu@gmail.com>
2024-03-24 17:05:48 -0700

    Basic UI

--- Assets/Script/LevelSelection.cs
+++ Assets/Script/LevelSelection.cs
@@ -7,18 +7,18 @@ public class LevelSelection : MonoBehaviour
 {
     public void OnButton1()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("Level0");
     }
 
     public void OnButton2()
     {
-        SceneManager.LoadScene("Level2");
+        SceneManager.LoadScene("Level1");
     }
 
-    //public void OnButton3()
-    //{
-    //    SceneManager.LoadScene("Level3");
-    //}
+    public void OnButton3()
+    {
+        SceneManager.LoadScene("Level2");
+    }
     //public void OnButton4()
     //{
     //    SceneManager.LoadScene("Level4");
