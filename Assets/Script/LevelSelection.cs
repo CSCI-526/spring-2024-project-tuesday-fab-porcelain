f96979df241b5da160d5ef9a3300fc8ca49e23ec
gxy <1195889430@qq.com>
2024-04-02 01:58:20 -0700

    'mergev3'

--- Assets/Script/LevelSelection.cs
+++ Assets/Script/LevelSelection.cs
@@ -15,12 +15,20 @@ public class LevelSelection : MonoBehaviour
         SceneManager.LoadScene("Level2");
     }
 
-    //public void OnButton3()
-    //{
-    //    SceneManager.LoadScene("Level3");
-    //}
-    //public void OnButton4()
-    //{
-    //    SceneManager.LoadScene("Level4");
-    //}
+    public void OnButton3()
+    {
+       SceneManager.LoadScene("Level3");
+    }
+    public void OnButton4()
+    {
+       SceneManager.LoadScene("Level4");
+    }
+    public void OnButton5()
+    {
+       SceneManager.LoadScene("Level5");
+    }
+    public void OnButton6()
+    {
+       SceneManager.LoadScene("Level6");
+    }
 }
