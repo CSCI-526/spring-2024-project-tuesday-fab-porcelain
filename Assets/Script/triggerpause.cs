ecb499c6355820f168650154470ffd243e2fef62
gxy <1195889430@qq.com>
2024-04-22 18:52:33 -0700

    add pause button

---
+++ Assets/Script/triggerpause.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+public class triggerpause : MonoBehaviour
+{
+    private int trigger=1;
+    public Button pauseButton;
+    public TextMeshProUGUI buttonText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = pauseButton.GetComponent<Button>();
+		btn.onClick.AddListener(OnPauseButton);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnPauseButton()
+    {
+        if (trigger==1){
+            Time.timeScale = 0;
+            buttonText.text = "CONTINUE";
+            Debug.Log("Pause");
+        }
+        else{
+            Time.timeScale = 1;
+            buttonText.text = "PAUSE";
+            Debug.Log("Continue");
+        }
+        trigger=1-trigger;
+    }
+}
